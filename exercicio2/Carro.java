c3cbbc090223161bf8fa41945fa4e804449f7d4e
Lazaro <lazarosobreira@gmail.com>
2021-02-11 01:44:23 +0000

    teste 4

---
+++ exercicio2/Carro.java
@@ -0,0 +1,15 @@
+
+class Carro{
+
+String cor;
+String marca;
+String modelo;
+int ano;
+
+void andar(float distancia){
+
+System.out.println("O carro andou" + distancia + "Km");
+
+}
+
+}
---
+++ exercicio2/Main.java
@@ -0,0 +1,16 @@
+
+class Main{
+
+  public static void main(String[] args){
+
+    Carro c1 = new Carro();
+    c1.marca = "Chevrolet";
+    c1.modelo = "Celta";
+    c1.cor = "Preto";
+    c1.ano = 2015;
+
+    c1.andar(13.5);
+
+  }
+
+}
