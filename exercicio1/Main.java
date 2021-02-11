d332921e587bb013143adadc8637e2e651de12fa
Lazaro <lazarosobreira@gmail.com>
2021-02-11 01:19:23 +0000

    teste 2

--- exercicio1/Main.java
+++ exercicio1/Main.java
@@ -3,7 +3,16 @@ class Main{
 
   public static void main(String args[]){
 
-    System.out.println("Olá, mundo!");
+    // Instanciando cachorros
+    Cachorro c1 = new Cachorro();
+    Cachorro c2 = new Cachorro();
+
+    // Atribuindo atributos
+    c1.nome = "Fofo";
+    c1.idade = 5;
+
+    c2.nome = "Bily";
+    c2.idade = 4;
 
   }
 
