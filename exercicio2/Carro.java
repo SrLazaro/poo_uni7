dd2279b6f3c9fafc667d290b03a4a548a960ac44
repl.it user <replituser@example.com>
2021-02-15 23:32:06 +0000

    POO Certo!

--- exercicio2/Carro.java
+++ exercicio2/Carro.java
@@ -1,14 +1,38 @@
-
 class Carro{
 
 String cor;
 String marca;
 String modelo;
+float tanque;
 int ano;
 
 void andar(float distancia){
 
-System.out.println("O carro andou" + distancia + "Km");
+float gasolina_gasta;
+
+// 1L = 12KM
+
+gasolina_gasta = distancia / 12;
+
+if(tanque >= gasolina_gasta){
+
+System.out.println("O carro andou " + distancia + " Km");
+
+tanque = tanque - gasolina_gasta;
+
+}else{
+
+  System.out.println("Precisa colocar mais gasolina para andar essa distância!");
+
+}
+
+}
+
+void abastecer(int litros){
+
+  System.out.println("Carro abastecido!");
+
+  tanque = tanque + litros;
 
 }
 
--- exercicio2/Main.java
+++ exercicio2/Main.java
@@ -1,16 +1,15 @@
-
-class Main{
-
-  public static void main(String[] args){
+class Main {
+  public static void main(String[] args) {
 
     Carro c1 = new Carro();
     c1.marca = "Chevrolet";
     c1.modelo = "Celta";
+    c1.tanque = 1;
     c1.cor = "Preto";
     c1.ano = 2015;
 
-    c1.andar(13.5);
-
+    c1.andar(24);
+    c1.abastecer(2);
+    c1.andar(24);
   }
-
 }
